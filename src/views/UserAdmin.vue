5f82ddbda68d13f98ec2c2c5cf7dfe79584428b3
nong99 <nong99@outlook.com>
2017-06-24 18:18:08 +0800

    up

--- src/views/UserAdmin.vue
+++ src/views/UserAdmin.vue
@@ -15,18 +15,18 @@
                     </CellBox>
 
                     <group class="group group-list" gutter="10px">
-                        <cell title="行者报名卡" is-link  link="user/medal">
+                        <cell title="行者报名卡" is-link @click.native="">
                         </cell>
-                        <cell title="运动装备设置" is-link>
+                        <cell title="运动装备设置" is-link @click.native="">
                         </cell>
                     </group>
 
                     <group class="group group-list" gutter="10px">
-                        <cell title="手机号" value="点击绑定手机号码" is-link  link="user/medal">
+                        <cell title="手机号" value="点击绑定手机号码" is-link @click.native="phoneDoalogFun()">
                         </cell>
-                        <cell title="邮箱" value="点击绑定邮箱" is-link>
+                        <cell title="邮箱" value="点击绑定邮箱" is-link @click.native="">
                         </cell>
-                        <cell title="密码" value="修改密码" is-link>
+                        <cell title="密码" value="修改密码" is-link @click.native="">
                         </cell>
                     </group>
 
@@ -52,30 +52,65 @@
                 </div>
             </div>
         </div>
+        <!--手机-->
+        <x-dialog v-model="isPhone">
+            <header class="top_hd">
+                <x-header :left-options="{showBack: false}">手机绑定</x-header>
+            </header>
+            <div class="main_bd">
+                <group class="phone-group">
+                    <x-input placeholder="请输入手机号" class="input-phone">
+                        <span slot="label" class="iconfont icon-phone"></span>
+                    </x-input>
+                    <x-input placeholder="请输入验证码" class="input-code">
+                        <span slot="label" class="iconfont icon-code"></span>
+                    </x-input>
+                </group>
+                <div style="margin: auto 10px;">
+                    <x-button type="warn">退出</x-button>
+                </div>
+            </div>
+        </x-dialog>  
+
+        <!--密码-->
+        <x-dialog v-model="isPassword">
+        </x-dialog>  
+
+        <!--邮箱-->
+        <x-dialog v-model="isEmail">
+        </x-dialog>  
     </div>
 </template>
 
 <script>
 import {
-    XHeader, XButton,
+    XHeader, XButton, XDialog, XInput,
     Group, Cell, CellBox,
     Grid, GridItem
 } from 'vux'
 export default {
     data () {
         return {
+            'isPhone': false,
+            'isPassword': false,
+            'isEmail': false
         }
-    },
-    methods: {
     },
     components: {
         XHeader,
         XButton,
+        XDialog,
+        XInput,
         Group,
         Cell,
         CellBox,
         Grid,
         GridItem
+    },
+    methods: {
+        phoneDoalogFun () {
+            this.isPhone = true
+        }
     },
     mounted () {
     }
@@ -83,7 +118,7 @@ export default {
 </script>
 
 <style lang="less" scoped>
-@import '//at.alicdn.com/t/font_8gdtvhipwc9885mi.css';
+@import '//at.alicdn.com/t/font_kezx2khkain0o1or.css';
 // ++ userAdmin 
 // ++ Class
 .page-content {
@@ -132,6 +167,7 @@ export default {
     font-size: 14px;
     box-sizing: border-box;
 }
+.u-info .text-Uinfo .my-addre { margin-top: 10px; }
 
 // group-list
 .main_bd .content-box > .group-list .weui-cell {
@@ -157,4 +193,36 @@ export default {
     font-size: 30px;
     line-height: 1;
 }
+
+
+// 弹层
+.vux-x-dialog .main_bd {
+    position: absolute;
+    width: 100%;
+    top: 50px; right: 0;
+    bottom: 0; left: 0;
+    background-color: #f2f2f2;
+}
+// 手机绑定
+.phone-group .iconfont {
+    font-size: 20px;
+    color: #ccc;
+    margin-right: 10px
+}
+.phone-group .input-code::before {
+    left: 45px;
+}
+
 </style>
+<style>
+.vux-x-dialog > .weui-dialog {
+    width: 100%; height: 100%;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
+    transform: translate(0,0);
+    max-width: 100%;
+}
+</style>
+
